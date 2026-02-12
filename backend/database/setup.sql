584f7ca53f98c3dd76036290844cbc7db8594d90
Beamlak <bam439@drexel.edu>
2026-02-12 00:51:03 -0500

    added server with login and signup apis

---
+++ backend/database/setup.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  hashed_password TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
